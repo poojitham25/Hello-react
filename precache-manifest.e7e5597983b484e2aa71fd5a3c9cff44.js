59655faba574f6a12541e00b0c596b963b764792
poojitha <poojitha.m@tadigital.com>
2020-02-26 18:15:27 +0530

    Updates

---
+++ precache-manifest.e7e5597983b484e2aa71fd5a3c9cff44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77f938d6165b044656d43453dd9f7923",
+    "url": "/React-todo-app-components/index.html"
+  },
+  {
+    "revision": "f36b8d547e3c7871d386",
+    "url": "/React-todo-app-components/static/css/main.cec0289a.chunk.css"
+  },
+  {
+    "revision": "76bed7ca0fb86c9f69b6",
+    "url": "/React-todo-app-components/static/js/2.1753260b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-todo-app-components/static/js/2.1753260b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f36b8d547e3c7871d386",
+    "url": "/React-todo-app-components/static/js/main.fb922a32.chunk.js"
+  },
+  {
+    "revision": "d58036aedda16b685789",
+    "url": "/React-todo-app-components/static/js/runtime-main.3eca2ee7.js"
+  }
+]);
